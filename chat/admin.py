805bfa17e9201c8065c0198408dec1efc83cb590
Juri Sajzew <j.sajzew@outlook.de>
2024-04-09 17:37:42 +0200

    created message for admin

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Message
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('created_ad', 'author', 'text', 'receiver')
+    search_fields = ('text',)
 
 # Register your models here.
+admin.site.register(Message, MessageAdmin)
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,16 @@
+from datetime import date
+from django.conf import settings
 from django.db import models
 
+
+
 # Create your models here.
+class Message(models.Model):
+    text = models.CharField(max_length=500)
+    created_ad = models.DateField(default=date.today)
+    #chat = muss noch zugewiesen werden
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_message_set')
+        
+    
+    
