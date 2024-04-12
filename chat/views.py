be62d5d3d681187cbd217cc2b332b618be6a8c4d
Juri Sajzew <j.sajzew@outlook.de>
2024-04-12 12:35:41 +0200

    update 12.04.2024

--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from .models import Chat, Message
+from django.contrib.auth import authenticate
 
 # Create your views here.
 def index(request):
     if request.method == "POST":
         print("Received data " + request.POST['textmessage']) # mit dem request.POST wird die Nachricht aus dem Input Feld aus dem HTML Teil rausgegeben
-        Message.objects.create(text=request.POST['textmessage'], chat=None, author=request.user, receiver=request.user)
-    
-    return render(request, 'chat/index.html') #hiermit wird die Seite neu gerendert
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+    chatMessages = Message.objects.filter(chat__id=1)
+    return render(request, 'chat/index.html', {'messages': chatMessages}) #hiermit wird die Seite neu gerendert
+
+def logIn(request):
+    user = authenticate(username="john", password="secret")
+    if user is not None:
+    # A backend authenticated the credentials
+        ...
+    else:
+    # No backend authenticated the credentials
+        ...
